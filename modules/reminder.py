5145515d176a41847a8fb9107644ff59d0c138ee
shreyans-gits <shreyans.sahu07@gmail.com>
2026-06-14 19:49:37 +0530

    Add reminder module with background notifications

---
+++ modules/reminder.py
@@ -0,0 +1,19 @@
+from threading import Timer
+from plyer import notification
+
+class ReminderModule:
+    def notify(self, message : str):
+        notification.notify(
+            title = "ZORO",
+            message = message
+        )
+
+    def set_notification(self, message : str, minutes : int) -> str:
+        timer = Timer(
+            minutes*60,
+            self.notify,
+            args = [message]
+        )
+
+        timer.start()
+        return f"Reminder set for {minutes} minutes from now."
